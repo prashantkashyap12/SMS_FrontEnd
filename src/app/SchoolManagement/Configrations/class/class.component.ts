a684e30beb839fe11b0677c28e51d56a5684d3b9
prashantkashyap12 <prashantkashyap12@gmail.com>
2026-04-09 08:25:36 +0530

    ui P1

--- src/app/SchoolManagement/Configrations/class/class.component.ts
+++ src/app/SchoolManagement/Configrations/class/class.component.ts
@@ -1,12 +1,54 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-class',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './class.component.html',
   styleUrl: './class.component.css'
 })
 export class ClassComponent {
+
+
+  // Basic definations
+  classForm!:FormGroup
+  constructor(private _fb:FormBuilder){}
+  ngOnInit(){
+    this.Init()
+  }
+
+
+  // Inislization Form
+  Init(){
+    this.classForm = this._fb.group({
+      ClassName: ['',Validators.required],
+      classRoman: ['', Validators.required]
+    })
+  }
+
+  // Allot Methrod
+
+
+  // Delete Methord
+  delete(){
+    
+  }
+
+  // OnSubmit Method
+  onSubmit(){
+    if(!this.classForm.valid){
+      alert("Form invalid Filled");
+    }
+
+  }
+
+  // Clear Form
+  clear(){
+    this.ngOnInit();
+  }
+
 
 }
---
+++ src/app/SchoolManagement/Configrations/config.service.ts
@@ -0,0 +1,124 @@
+import { Injectable } from '@angular/core';
+import { urls } from '../../common/common';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+
+  constructor(private _http:HttpClient) { }
+  private baseUrls = new urls().webApiUrl;
+ 
+ 
+  // Add class -- OPEN
+  viewClass():Observable<any>{
+    let url = this.baseUrls;
+    return this._http.get(url);
+  }
+  addClass(model:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.post(url, model);
+  }
+  delClass(classId:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.delete(url,classId);
+  }
+  updateClass(model:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.post(url, model);
+  }
+
+  // Add Academic -- OPEN
+  viewAcademic():Observable<any>{
+    let url = this.baseUrls;
+    return this._http.get(url);
+  }
+  addAcademic(model:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.post(url, model);
+  }
+  delAcademic(AcademicId:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.delete(url,AcademicId);
+  }
+  updateAcademic(model:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.post(url, model);
+  }
+
+    // Add Section -- OPEN
+  viewSection():Observable<any>{
+    let url = this.baseUrls;
+    return this._http.get(url);
+  }
+  addSection(model:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.post(url, model);
+  }
+  delSection(SectionId:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.delete(url,SectionId);
+  }
+  updateSection(model:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.post(url, model);
+  }
+
+  // GenClass (Class + Acadmic + Section)
+  viewGenClass():Observable<any>{
+    let url = this.baseUrls;
+    return this._http.get(url);
+  }
+  addGenClass(model:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.post(url, model);
+  }
+  delGenClass(GenClassId:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.delete(url,GenClassId);
+  }
+  updateGenClass(model:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.post(url, model);
+  }
+
+  // Add Fee
+   viewFee():Observable<any>{
+    let url = this.baseUrls;
+    return this._http.get(url);
+  }
+  addFee(model:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.post(url, model);
+  }
+  delFee(FeeId:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.delete(url,FeeId);
+  }
+  updateFee(model:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.post(url, model);
+  }
+
+  // Fee Stracture 
+  viewFeeStract():Observable<any>{
+    let url = this.baseUrls;
+    return this._http.get(url);
+  }
+  addFeeStract(model:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.post(url, model);
+  }
+  delFeeStract(FeeStractId:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.delete(url,FeeStractId);
+  }
+  updateFeeStract(model:any):Observable<any>{
+    let url = this.baseUrls;
+    return this._http.post(url, model);
+  }
+
+
+}
